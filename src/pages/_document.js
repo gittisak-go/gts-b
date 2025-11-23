4b1155f085026c8d6532e92e934f2b1527b8e3cc
gittisak-go <gittisakwannakeeree@gmail.com>
2025-11-23 09:13:01 +0700

    chore: add favicon link and msapplication/theme-color meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,9 @@ export default function Document() {
     <Html lang="th">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#1b1b1b" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
