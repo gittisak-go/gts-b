615b265ed7e9a06a97f37f0aaa8522bdd06744c4
gittisak-go <gittisakwannakeeree@gmail.com>
2025-11-23 08:40:28 +0700

    chore: add viewport meta and set lang=th for mobile compatibility

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="th">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#1b1b1b" />
+      </Head>
       <body>
         <Main />
         <NextScript />
